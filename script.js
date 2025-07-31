56f3b9800235ddf1d54e5e25680358bcda29f672
MichaelWaterfall <michaeljames.waterfall@gmail.com>
2025-07-31 14:29:27 +0100

    array not showing fixed

--- script.js
+++ script.js
@@ -1,11 +1,21 @@
 const myLibrary = [];
 
+//const addBook = document.getElementById("addBookButton");
+
+
 function Book(title, author, pages) {
     this.bookTitle = title;
     this.bookAuthor = author;
     this.bookPages = pages;
 }
 
-function addBookToLibrary(title, author, pages) {
-    myLibrary.push(new Book(title, author, pages));
+
+document.getElementById("addBookButton").onclick = function (){
+    //console.log(title + author + pages);
+    const authorInput = document.getElementById("authorInput").value;
+    const titleInput = document.getElementById("titleInput").value;
+    const pagesInput = document.getElementById("pagesInput").value;
+    myLibrary.push(new Book(authorInput, titleInput, pagesInput));
+    console.log(myLibrary);
+    console.log("update");
 }
