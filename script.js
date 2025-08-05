59e3ecfeb0598908e86eedd0de16ddc32945748d
MichaelWaterfall <michaeljames.waterfall@gmail.com>
2025-08-05 18:53:21 +0100

    displayBooks function

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 const myLibrary = [];
-
+//const mainDiv = document.getElementById("main");
 //const addBook = document.getElementById("addBookButton");
 
 
@@ -12,7 +12,27 @@ function Book(title, author, pages) {
 
 function displayBooks() {
     for(let i = 0; i < myLibrary.length; i++) {
+        const newDiv = document.createElement("div");
+        const titleDiv = document.createElement("div");
+        const authorDiv = document.createElement("div");
+        const pagesDiv = document.createElement("div");
+        const idDiv = document.createElement("div");
+        const readButton = document.createElement("button");
+        const removeButton = document.createElement("button");
+        newDiv.id = "library-div";
+        titleDiv.innerHTML = myLibrary[i].bookTitle;
+        authorDiv.innerHTML = "author";
+        pagesDiv.innerHTML = "pages";
+        idDiv.innerHTML = "id"
         
+        document.getElementById("main").appendChild(newDiv);
+        document.getElementById("library-div").appendChild(titleDiv);
+        document.getElementById("library-div").appendChild(authorDiv);
+        document.getElementById("library-div").appendChild(pagesDiv);
+        document.getElementById("library-div").appendChild(idDiv);
+        document.getElementById("library-div").appendChild(readButton);
+        document.getElementById("library-div").appendChild(removeButton);
+        console.log("div");
     }
 }
 
