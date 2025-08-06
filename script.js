058ff15d5e7eaf418f6b14f7577996d803080e73
MichaelWaterfall <michaeljames.waterfall@gmail.com>
2025-08-06 19:32:12 +0100

    layout updated

--- script.js
+++ script.js
@@ -8,43 +8,64 @@ function Book(title, author, pages) {
     this.bookAuthor = author;
     this.bookPages = pages;
     this.id = crypto.randomUUID();
+    this.read = false;
 }
+
+Book.prototype.remove = function () {
+    const index = myLibrary.findIndex(book => book.id === this.id);
+    if (index !== -1) {
+        myLibrary.splice(index, 1);
+    }
+};
 
 function displayBooks() {
     const booksContainer = document.getElementById("books-container");
     booksContainer.innerHTML = "";
 
     for (let i = 0; i < myLibrary.length; i++) {
+        const book = myLibrary[i];
+
         const newDiv = document.createElement("div");
         const titleDiv = document.createElement("div");
         const authorDiv = document.createElement("div");
         const pagesDiv = document.createElement("div");
         const idDiv = document.createElement("div");
+        const readDiv = document.createElement("div");
         const readButton = document.createElement("button");
         const removeButton = document.createElement("button");
-        
-        removeButton.onclick = function () {
-            console.log(myLibrary[i]);
-        };
 
         newDiv.classList.add("library-div");
-        readButton.innerHTML = "Read";
+
+        titleDiv.innerHTML = "Title: " + book.bookTitle;
+        authorDiv.innerHTML = "Author: " + book.bookAuthor;
+        pagesDiv.innerHTML = "Pages: " + book.bookPages;
+        idDiv.innerHTML = "ID: " + book.id;
+        readDiv.innerHTML = "Read: " + (book.read ? "✅ Yes" : "❌ No");
+
+        readButton.innerHTML = "Toggle Read";
+        readButton.onclick = function () {
+            book.read = !book.read;
+            displayBooks();
+        };
+
         removeButton.innerHTML = "Remove";
-        titleDiv.innerHTML = "Title: " + myLibrary[i].bookTitle;
-        authorDiv.innerHTML = "Author: " + myLibrary[i].bookAuthor;
-        pagesDiv.innerHTML = "Pages: " + myLibrary[i].bookPages;
-        idDiv.innerHTML = "Id: " + myLibrary[i].id;
+        removeButton.onclick = function () {
+            book.remove();
+            displayBooks();
+        };
 
         newDiv.appendChild(titleDiv);
         newDiv.appendChild(authorDiv);
         newDiv.appendChild(pagesDiv);
         newDiv.appendChild(idDiv);
+        newDiv.appendChild(readDiv);
         newDiv.appendChild(readButton);
         newDiv.appendChild(removeButton);
 
         booksContainer.appendChild(newDiv);
     }
 }
+
 
 
 
